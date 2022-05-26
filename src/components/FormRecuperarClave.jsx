007cf03fabd2efd772cde6759b0712ed84166134
MatiasMinig <matiasminig@gmail.com>
2022-05-26 01:27:06 -0300

    Pagina Recuperar Contraseña

--- src/components/FormRecuperarClave.jsx
+++ src/components/FormRecuperarClave.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const FormRecuperarClave = () => {
   return (
-    <div>FormRecuperarClave</div>
-  )
-}
+    <>
+      <div className="xl:h-screen lg:h-auto">
+        <div className="container px-6 py-12 h-full">
+          <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+            <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                className="w-full"
+                aria-hidden
+                alt="Picture of me taking a photo of an image"   
+              />
+            </div>
 
-export default FormRecuperarClave
+            <div className="p-6 rounded-lg shadow-lg bg-white max-w-md  flex justify-end lg:mt-8 md:mt-5">
+              <form>
+                <div className="form-group mb-6 lg:w-80 sm:w-44">
+                  <input
+                    type="email"
+                    className="form-control block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    id="email"
+                    placeholder="Email"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="
+      w-full
+      px-6
+      py-2.5
+      bg-green-600
+      text-white
+      font-medium
+      text-xs
+      leading-tight
+      uppercase
+      rounded
+      shadow-md
+      hover:bg-green-700 hover:shadow-lg
+      focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0
+      active:bg-green-800 active:shadow-lg
+      transition
+      duration-150
+      ease-in-out"
+                >
+                  Recuperar Contraseña
+                </button>
+
+                <NavLink to="/">
+                  <button
+                    type="submit"
+                    className="
+      w-full
+      px-6
+      py-2.5
+      mt-3
+      bg-blue-600
+      text-white
+      font-medium
+      text-xs
+      leading-tight
+      uppercase
+      rounded
+      shadow-md
+      hover:bg-blue-700 hover:shadow-lg
+      focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+      active:bg-blue-800 active:shadow-lg
+      transition
+      duration-150
+      ease-in-out"
+                  >
+                    Volver
+                  </button>
+                </NavLink>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FormRecuperarClave;
--- src/pages/RecuperarClave.jsx
+++ src/pages/RecuperarClave.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import FormRecuperarClave from "../components/FormRecuperarClave";
 
 const RecuperarClave = () => {
   return (
-    <h1 className="bg-red-500">Recuperar Clave</h1>
-  )
-}
+    <>
+      <div
+        className="bg-cover bg-no-repeat bg-center  xl:h-full lg:h-full md:h-full"
+        style={{ backgroundImage: "url('img/viaje11.png')" }}
+      >
+        <div className="grid grid-cols-1 text-center text-white px-12 md:px-12">
+          <h1 className="font-Amatic hover:text-teal-400 text-8xl font-black mt-10">
+            Recuperar Clave
+          </h1>
+        </div>
+        <FormRecuperarClave />
+      </div>
+    </>
+  );
+};
 
-export default RecuperarClave
+export default RecuperarClave;
