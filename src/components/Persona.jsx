78035dc9e49f5b3c01aecfd08290cffd1a70fcd6
Matias Minig <matiasminig@gmail.com>
2022-09-11 02:56:20 -0300

    boton-volver

--- src/components/Persona.jsx
+++ src/components/Persona.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
+
 const Persona = () => {
   return (
     <section className="mb-20 text-gray-700">
--- src/pages/QuienesSomos.jsx
+++ src/pages/QuienesSomos.jsx
@@ -1,21 +1,33 @@
 import Persona from "../components/Persona";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const QuienesSomos = () => {
   return (
     <>
-       <div
+      <div
         className="bg-cover bg-no-repeat bg-center"
         style={{ backgroundImage: "url('img/viaje7.png')" }}
       >
         <div className="grid grid-cols-1 text-center text-black px-12 md:px-12">
+          <div className="flex justify-between mt-5">
+            <Link to="/">
+              <FontAwesomeIcon
+                className="w-10 h-10 text-black bg-white rounded-full hover:text-teal-500"
+                icon={faCircleArrowLeft}
+              />
+            </Link>
+          </div>
           <h1 className="font-Amatic hover:text-teal-400 lg:text-8xl md:text-6xl font-black mt-10 text-5xl">
             Quienes Somos
           </h1>
+
           <div className="grid grid-cols-1 gap-6 text-center hover:text-teal-400 text-white px-12 md:px-12 rounded-2xl mb-20 mt-16">
-            <Persona/>
+            <Persona />
           </div>
         </div>
-      </div> 
+      </div>
     </>
   );
 };
