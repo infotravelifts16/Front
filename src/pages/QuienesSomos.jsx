c4829d213a49f2a3212f9f79a992d762ea8dd978
Matias Minig <matiasminig@gmail.com>
2022-09-11 02:19:42 -0300

    Quienes somos finalizado

--- src/pages/QuienesSomos.jsx
+++ src/pages/QuienesSomos.jsx
@@ -1,3 +1,5 @@
+import Persona from "../components/Persona";
+
 const QuienesSomos = () => {
   return (
     <>
@@ -5,16 +7,12 @@ const QuienesSomos = () => {
         className="bg-cover bg-no-repeat bg-center"
         style={{ backgroundImage: "url('img/viaje7.png')" }}
       >
-        <div className="grid grid-cols-1 text-center text-white px-12 md:px-12">
-          <h1 className="font-Amatic hover:text-teal-400 text-8xl font-black mt-10">
+        <div className="grid grid-cols-1 text-center text-black px-12 md:px-12">
+          <h1 className="font-Amatic hover:text-teal-400 lg:text-8xl md:text-6xl font-black mt-10 text-5xl">
             Quienes Somos
           </h1>
-          <div className="grid grid-cols-1 text-center text-white px-12 md:px-12 ">
-            <p className="font-Amatic text-6xl font-black mt-10">
-              Somos un equipo de compañeros del Instituto de Formación Técnica
-              Superior N°16.Dentro del 3 año finalizando la carrera se realiza dentro de la materia Taller 3,
-              un trabajo final, en donde los alumnos decidimos desarrollar Infotravel.
-            </p>
+          <div className="grid grid-cols-1 gap-6 text-center hover:text-teal-400 text-white px-12 md:px-12 rounded-2xl mb-20 mt-16">
+            <Persona/>
           </div>
         </div>
       </div> 
